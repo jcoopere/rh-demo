868aa296544fbebefbad199f3c0c4094e31053bb
jcooperellis <jcooperellis@cloudera.com>
2017-09-07 16:04:43 -0400

    cleanup and override toString

--- streaming/src/main/scala/com/cloudera/demo/iiot/util/MaintenanceScheduler.scala
+++ streaming/src/main/scala/com/cloudera/demo/iiot/util/MaintenanceScheduler.scala
@@ -52,20 +52,7 @@ class MaintenanceScheduler(mqttBroker:String) extends Serializable {
 					val event = new MaintenanceEvent(eventId, description, timestamp, mType, reason, startTime, endTime)
 
 					publishEventOverMqtt(motorId, event)
-/*
-					// Send Kafka message
-					val props = new Properties
-			        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerList)
-			        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-			        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 
-			        val producer = new KafkaProducer[String, String](props)
-
-			        val message = new ProducerRecord[String, String](kafkaTopic, null, event.toJson())
-
-			        producer.send(message)
-			        producer.close()
-*/
 					softFailureScheduled = true
 				}
 			}
@@ -83,20 +70,7 @@ class MaintenanceScheduler(mqttBroker:String) extends Serializable {
 					val event = new MaintenanceEvent(eventId, description, timestamp, mType, reason, startTime, endTime)
 
 					publishEventOverMqtt(motorId, event)
-/*
-					// Send Kafka message
-					val props = new Properties
-			        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerList)
-			        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-			        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 
-			        val producer = new KafkaProducer[String, String](props)
-
-			        val message = new ProducerRecord[String, String](kafkaTopic, null, event.toJson())
-
-			        producer.send(message)
-			        producer.close()
-*/
 					hardFailureScheduled = true
 				}
 			}
@@ -121,28 +95,11 @@ class MaintenanceScheduler(mqttBroker:String) extends Serializable {
 			client.disconnect()
 		}
 	}
-}
-/*
-object MaintenanceScheduler {
-	implicit def publishEventOverMqtt(event:MaintenanceEvent) {
-		try {
-			val client = new MqttClient(mqttBroker, MqttClient.generateClientId, new MqttDefaultFilePersistence("/tmp"))
 
-			client.connect()
-
-			val msgTopic = client.getTopic(mqttTopic)
-			val msg = new MqttMessage(event.toJsonString.getBytes("utf-8"))
-
-			msgTopic.publish(msg)
-		} catch {
-			case e:MqttException => println(e)
-		} finally {
-			client.disconnect()
-		}
+	override def toString:String = {
+		s"MaintenanceScheduler($softFailureScheduled,$hardFailureScheduled)"
 	}
 }
-
-*/
 
 
 
